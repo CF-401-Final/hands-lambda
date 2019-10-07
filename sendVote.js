87cc657915e57c6192e62fab378b05561cd4ae48
trevorjdobson <31601148+trevorjdobson@users.noreply.github.com>
2019-10-07 10:57:55 -0700

    Update sendVote.js

--- sendVote.js
+++ sendVote.js
@@ -16,39 +16,73 @@ exports.handler = async (event, context, callback) => {
   console.log('event', event);
   let connectionData;
   
-  try {
-    connectionData = await ddb.scan({ TableName: TABLE_NAME, ProjectionExpression: 'connectionId' }).promise();
-  } catch (e) {
-    return { statusCode: 500, body: e.stack };
-  }
-  
-  console.log('connection data', connectionData);
-  
-  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
-    apiVersion: '2018-11-29',
-    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
-  });
-  
   const postData = JSON.parse(event.body).data;
   console.log('postData', postData);
   var putParams = {
     TableName: process.env.TABLE_NAME,
     Item: {
       connectionId: { S: event.requestContext.connectionId },
-      vote: { S: postData }
+      vote: { S: postData },
+      timeVoted: { S: Date.now().toString() }
     }
   };
 
-  DDB.putItem(putParams, function (err) {
+  await DDB.putItem(putParams, function (err) {
     callback(null, {
       statusCode: err ? 500 : 200,
       body: err ? "Failed to connect: " + JSON.stringify(err) : "Connected."
     });
   });
   
+  
+  
+  try {
+    connectionData = await ddb.scan({ TableName: TABLE_NAME}).promise();
+  } catch (e) {
+    return { statusCode: 500, body: e.stack };
+  }
+  console.log('connection data', connectionData);
+   //Tally up the votes
+   let data = {
+     zeroCount: 0,
+     oneCount: 0,
+     twoCount: 0,
+     threeCount: 0,
+     fourCount: 0,
+     fiveCount: 0,
+     totalConnections: 0
+   }
+  
+   connectionData.Items.forEach(connection =>{
+     if(connection.vote){
+       if(connection.vote === "0"){
+         data.zeroCount++;
+       }else if(connection.vote === "1"){
+         data.oneCount++;
+       }else if(connection.vote === "2"){
+         data.twoCount++;
+       }else if(connection.vote === "3"){
+         data.threeCount++;
+       }else if(connection.vote === "4"){
+         data.fourCount++;
+       }else if(connection.vote === "5"){
+         data.fiveCount++;
+       }
+     }
+   })
+  data.totalConnections = connectionData.Count;
+     console.log('data',data);
+   
+  const apigwManagementApi = new AWS.ApiGatewayManagementApi({
+    apiVersion: '2018-11-29',
+    endpoint: event.requestContext.domainName + '/' + event.requestContext.stage
+  });
+  
+ 
+  let finalData = JSON.stringify(data);
   const postCalls = connectionData.Items.map(async ({ connectionId }) => {
     try {
-      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: postData }).promise();
+      await apigwManagementApi.postToConnection({ ConnectionId: connectionId, Data: finalData }).promise();
     } catch (e) {
       if (e.statusCode === 410) {
         console.log(`Found stale connection, deleting ${connectionId}`);
